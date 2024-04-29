255de9b0991082338f7ad96f2bbe210bde78639d
Josh <jd52981@gmail.com>
2024-04-29 17:52:23 +0000

    first push

---
+++ pages/profile/index.tsx
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+function Profile() {
+    return (
+        <div>
+            <Typography variant="h1" component="h2">
+                Profile
+            </Typography>
+        </div>
+    )
+}
+
+export default Profile;
