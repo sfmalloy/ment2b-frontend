65e768de08b1fcffae2b348ba86b24a2e42b139e
Sean Malloy <techfan321@gmail.com>
2024-04-30 16:13:38 -0400

    autocomplete and stuff

--- components/SkillSet.tsx
+++ components/SkillSet.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, Grid, List, ListItem, ListItemButton, TextField } from '@mui/material';
-import { useRef, useState } from 'react';
+import { Autocomplete, Box, Button, Grid, List, ListItem, ListItemButton, TextField } from '@mui/material';
+import { useEffect, useRef, useState } from 'react';
 
 type SkillSetProps = {
   onChange: (skills: string[]) => void;
@@ -9,6 +9,8 @@ export function SkillSet({ onChange }: SkillSetProps) {
   const [skills, setSkills] = useState<string[]>([]);
   const [textboxValue, setTextboxValue] = useState<string>('');
   const ref = useRef<HTMLInputElement>();
+  const [autocompleteValues, setAutocompleteValues] = useState<string[]>([]);
+  const [autocompleteDelay, setAutocompleteDelay] = useState<NodeJS.Timeout>();
 
   const addSkill = (e: any, value: string, preventDefault: boolean = true) => {
     if (!skills.includes(value)) {
@@ -25,22 +27,50 @@ export function SkillSet({ onChange }: SkillSetProps) {
   return (
     <>
       <Grid item xs={4}>
-        <TextField
-          inputRef={ref}
-          fullWidth
-          value={textboxValue}
-          label='Skills'
-          autoComplete='off'
-          onKeyDown={(e) => {
-            const target = e.target as HTMLInputElement;
-            if (e.key.toLowerCase() === 'enter') {
-              addSkill(e, target.value);
-            }
-          }}
-          onInput={(e) => {
-            const target = e.target as HTMLInputElement;
-            setTextboxValue(target.value);
-          }}
+        <Autocomplete
+          options={autocompleteValues}
+          popupIcon={null}
+          clearIcon={null}
+          filterOptions={(autocompleteValues) => autocompleteValues}
+          freeSolo
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              inputRef={ref}
+              fullWidth
+              value={textboxValue}
+              label='Skills'
+              autoComplete='off'
+              onKeyDown={(e) => {
+                const target = e.target as HTMLInputElement;
+                if (e.key.toLowerCase() === 'enter') {
+                  addSkill(e, target.value);
+                }
+              }}
+              onChange={(e) => {
+                if (autocompleteDelay) {
+                  clearTimeout(autocompleteDelay);
+                  setAutocompleteDelay(undefined);
+                }
+
+                const skill = e.target.value;
+                if (skill.length > 0) {
+                  const timeoutId = setTimeout(() => {
+                    fetch('http://localhost:8080/skills?' + new URLSearchParams({ skillSubstring: skill }))
+                      .then((res) => res.json())
+                      .then((json) => setAutocompleteValues(json));
+                  }, 100);
+                  setAutocompleteDelay(timeoutId);
+                } else {
+                  setAutocompleteValues([]);
+                }
+              }}
+              onInput={(e) => {
+                const target = e.target as HTMLInputElement;
+                setTextboxValue(target.value);
+              }}
+            />
+          )}
         />
         <Button
           fullWidth
