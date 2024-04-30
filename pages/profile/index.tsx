8c3a38348b9fa4e3040c2c54006f7c5c34d90b94
Josh <jd52981@gmail.com>
2024-04-30 00:47:52 +0000

    updates

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,11 +1,17 @@
-import { Box, CardContent, Collapse, List, ListItemButton, ListItemIcon, ListItemText, ListSubheader, Typography } from '@mui/material'
+import { Box, Grid, Collapse, List, ListItem, ListItemButton, ListItemIcon, ListItemText, ListSubheader, Typography } from '@mui/material'
 import ExpandLess from '@mui/icons-material/ExpandLess'
 import ExpandMore from '@mui/icons-material/ExpandMore'
 import * as React from 'react'
 
+function generate(list: String[], element: React.ReactElement){
+    return list.map((value) => 
+        React.cloneElement(element, {
+            value
+        })
+    )
+}
 
-
-function Profile() {
+function Profile(userInfo: any) {
     
     const [open, setOpen] = React.useState(true);
 
@@ -19,73 +25,83 @@ function Profile() {
                 Profile
             </Typography>
             <React.Fragment>
-                <CardContent>
-                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                        Personal Info
-                    </Typography>
-                    <List
-                        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}
-                        component="nav"
-                        aria-labelledby='nested-list-subheader'
-                        subheader={
-                            <ListSubheader component="div" id="nested-list-subheader">
-                                Nested List Items
-                            </ListSubheader>
-                        }
-                    >
-                        <ListItemButton>
-                            <ListItemText primary="First Name" />
-                        </ListItemButton>
-                        <ListItemButton>
-                            <ListItemText primary="Last Name" />
-                        </ListItemButton>
-                        <ListItemButton>
-                            <ListItemText primary="Email" />
-                        </ListItemButton>
-                        <ListItemButton>
-                            <ListItemText primary="Grade" />
-                        </ListItemButton>
-                        <ListItemButton>
-                            <ListItemText primary="Position" />
-                        </ListItemButton>
-                        <ListItemButton>
-                            <ListItemText primary="Subdivision" />
-                        </ListItemButton>
-                        <ListItemButton onClick={handleClick}>
-                            <ListItemText primary="Skills" />
-                            {open ? <ExpandLess /> :<ExpandMore />}
-                        </ListItemButton>
-                        <Collapse in={open} timeout="auto" unmountOnExit>
-                            <List component="div" disablePadding>
-                                <ListItemButton sx={{ pl: 4 }}>
+                <Box sx={{ flexGrow: 1, maxWidth: 752}}>
+                    <Grid container spacing={2}>
+                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                            Personal Info
+                        </Typography>
+                        <List
+                            sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}
+                            component="nav"
+                            aria-labelledby='nested-list-subheader'
+                            subheader={
+                                <ListSubheader component="div" id="nested-list-subheader"></ListSubheader>
+                            }
+                        >
+                            <ListItem>
+                                <ListItemText primary="First Name" />
+                                { userInfo.firstName }
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Last Name" />
+                                { userInfo.lastName }
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Email" />
+                                { userInfo.email }
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Grade" />
+                                { userInfo.Grade }
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Position" />
+                                { userInfo.position }
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Subdivision" />
+                                { userInfo.subDivision }
+                            </ListItem>
+                            <ListItemButton onClick={handleClick}>
+                                <ListItemText primary="Skills" />
+                                {open ? <ExpandLess /> :<ExpandMore />}
+                            </ListItemButton>
+                            <Collapse in={open} timeout="auto" unmountOnExit>
+                                <List component="div" disablePadding>
+                                    <ListItemButton sx={{ pl: 4 }}>
 
-                                </ListItemButton>
-                            </List>
-                        </Collapse>
-                        <ListItemButton onClick={handleClick}>
-                            <ListItemText primary="Desired Skills" />
-                            {open ? <ExpandLess /> :<ExpandMore />}
-                        </ListItemButton>
-                        <Collapse in={open} timeout="auto" unmountOnExit>
-                            <List component="div" disablePadding>
-                                <ListItemButton sx={{ pl: 4 }}>
-                                    
-                                </ListItemButton>
-                            </List>
-                        </Collapse>
-                        <ListItemButton onClick={handleClick}>
-                            <ListItemText primary="Desired Grade" />
-                            {open ? <ExpandLess /> :<ExpandMore />}
-                        </ListItemButton>
-                        <Collapse in={open} timeout="auto" unmountOnExit>
-                            <List component="div" disablePadding>
-                                <ListItemButton sx={{ pl: 4 }}>
-                                    
-                                </ListItemButton>
-                            </List>
-                        </Collapse>
-                    </List>
-                </CardContent>
+                                    </ListItemButton>
+                                </List>
+                            </Collapse>
+                            <ListItemButton onClick={handleClick}>
+                                <ListItemText primary="Desired Skills" />
+                                {open ? <ExpandLess /> :<ExpandMore />}
+                            </ListItemButton>
+                            <Collapse in={open} timeout="auto" unmountOnExit>
+                                <List component="div" disablePadding>
+                                    {generate(
+                                        userInfo.desiredSkills,
+                                        <ListItem>
+                                            <ListItemText
+                                                primary="{value}"/>
+                                        </ListItem>
+                                    )}
+                                </List>
+                            </Collapse>
+                            <ListItemButton onClick={handleClick}>
+                                <ListItemText primary="Desired Grade" />
+                                {open ? <ExpandLess /> :<ExpandMore />}
+                            </ListItemButton>
+                            <Collapse in={open} timeout="auto" unmountOnExit>
+                                <List component="div" disablePadding>
+                                    <ListItemButton sx={{ pl: 4 }}>
+                                        
+                                    </ListItemButton>
+                                </List>
+                            </Collapse>
+                        </List>
+                    </Grid>    
+                </Box>
             </React.Fragment>
         </div>
     )
