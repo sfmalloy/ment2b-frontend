fc7fc588ea3da95d71e68959653b927e8dd552b1
Josh <jd52981@gmail.com>
2024-04-30 14:20:00 +0000

    update lists

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -3,21 +3,25 @@ import ExpandLess from '@mui/icons-material/ExpandLess'
 import ExpandMore from '@mui/icons-material/ExpandMore'
 import * as React from 'react'
 
-function generate(list: String[], element: React.ReactElement){
-    return list.map((value) => 
-        React.cloneElement(element, {
-            value
-        })
-    )
-}
-
 function Profile(userInfo: any) {
     
-    const [open, setOpen] = React.useState(true);
+    const [skills, showSkills] = React.useState(true);
+    const [dSkills, showDSkills] = React.useState(true)
+    const [grades, showGrades ] = React.useState(true)
 
-    const handleClick = () => {
-        setOpen(!open);
+    const handleSkillsClick = () => {
+        showSkills(!skills);
     }
+    const handleDSkillsClick = () => {
+        showDSkills(!dSkills);
+    }
+    const handleGradesClick = () => {
+        showGrades(!grades);
+    }
+
+    const dSkillsList = ["yes","no","something"];
+    const skillsList = ["skill1","skill2"];
+    const gradesList = ["grade1"];
 
     return (
         <div>
@@ -34,10 +38,7 @@ function Profile(userInfo: any) {
                             sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}
                             component="nav"
                             aria-labelledby='nested-list-subheader'
-                            subheader={
-                                <ListSubheader component="div" id="nested-list-subheader"></ListSubheader>
-                            }
-                        >
+                            subheader={ <ListSubheader component="div" id="nested-list-subheader"></ListSubheader> }>
                             <ListItem>
                                 <ListItemText primary="First Name" />
                                 { userInfo.firstName }
@@ -62,41 +63,43 @@ function Profile(userInfo: any) {
                                 <ListItemText primary="Subdivision" />
                                 { userInfo.subDivision }
                             </ListItem>
-                            <ListItemButton onClick={handleClick}>
+                            <ListItemButton onClick={handleSkillsClick}>
                                 <ListItemText primary="Skills" />
-                                {open ? <ExpandLess /> :<ExpandMore />}
+                                {skills ? <ExpandLess /> :<ExpandMore />}
                             </ListItemButton>
-                            <Collapse in={open} timeout="auto" unmountOnExit>
+                            <Collapse in={skills} timeout="auto" unmountOnExit>
                                 <List component="div" disablePadding>
-                                    <ListItemButton sx={{ pl: 4 }}>
-
-                                    </ListItemButton>
-                                </List>
-                            </Collapse>
-                            <ListItemButton onClick={handleClick}>
-                                <ListItemText primary="Desired Skills" />
-                                {open ? <ExpandLess /> :<ExpandMore />}
-                            </ListItemButton>
-                            <Collapse in={open} timeout="auto" unmountOnExit>
-                                <List component="div" disablePadding>
-                                    {generate(
-                                        userInfo.desiredSkills,
-                                        <ListItem>
-                                            <ListItemText
-                                                primary="{value}"/>
+                                    {skillsList.map((value: string) => 
+                                        <ListItem key={value} sx={{ pl: 6 }}>
+                                            <ListItemText primary={value}/>
                                         </ListItem>
                                     )}
                                 </List>
                             </Collapse>
-                            <ListItemButton onClick={handleClick}>
-                                <ListItemText primary="Desired Grade" />
-                                {open ? <ExpandLess /> :<ExpandMore />}
+                            <ListItemButton onClick={handleDSkillsClick}>
+                                <ListItemText primary="Desired Skills" />
+                                {dSkills ? <ExpandLess /> :<ExpandMore />}
                             </ListItemButton>
-                            <Collapse in={open} timeout="auto" unmountOnExit>
+                            <Collapse in={dSkills} timeout="auto" unmountOnExit>
                                 <List component="div" disablePadding>
-                                    <ListItemButton sx={{ pl: 4 }}>
-                                        
-                                    </ListItemButton>
+                                    {dSkillsList.map((value: string) => 
+                                        <ListItem key={value} sx={{ pl: 6 }}>
+                                            <ListItemText primary={value}/>
+                                        </ListItem>
+                                    )}
+                                </List>
+                            </Collapse>
+                            <ListItemButton onClick={handleGradesClick}>
+                                <ListItemText primary="Desired Grade" />
+                                {grades ? <ExpandLess /> :<ExpandMore />}
+                            </ListItemButton>
+                            <Collapse in={grades} timeout="auto" unmountOnExit>
+                                <List component="div" disablePadding>
+                                    {gradesList.map((value: string) => 
+                                        <ListItem key={value} sx={{ pl: 6 }}>
+                                            <ListItemText primary={value}/>
+                                        </ListItem>
+                                    )}
                                 </List>
                             </Collapse>
                         </List>
